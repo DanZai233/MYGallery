37d949d5694cc068978fe13bbe9738883f9f0430
DanZai233 <chz2001@126.com>
2025-11-06 11:46:39 +0800

    UPG

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # 使用传统构建方式启动服务（兼容旧版 buildx）
-DOCKER_BUILDKIT=0 COMPOSE_DOCKER_CLI_BUILD=0 docker compose up -d "$@"
+# 禁用 BuildKit 和 Bake 以避免兼容性问题
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
+export COMPOSE_EXPERIMENTAL_DOCKER_BUILDKIT=0
+docker compose up -d "$@"
 
